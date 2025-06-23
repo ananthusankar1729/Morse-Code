51c728c422c36fa5bc7631f91194816ad7529839
ananthusankar1729 <211710819+ananthusankar1729@users.noreply.github.com>
2025-06-23 10:29:13 +0000

    create function getComputerChoice

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -57,3 +57,81 @@ while (keepPlaying) {
     keepPlaying = confirm("Continue to next level?");
 }
 alert("Thanks for playing!");
+import Ka from './ka.js'
+function getComputerChoice(){
+    let m = Ka.mulberry32(1);
+    let x = m();
+    if (x<=(1/10)) {
+        let z = Ka.mulberry32(1)
+        let y = z()
+        let a = ["ART", "EAR", "TEA", "RAT", "SET"];
+        let b = ["A", "E", "T", "R", "S"];
+        if (y<=1/5){
+            b.push(a[0]);
+        }
+        else if (y<=2/5){
+            b.push(a[1]);
+        }
+        else if (y<=3/5){
+            b.push(a[2]);
+        }
+        else if (y<=4/5){
+            b.push(a[3]);
+        }
+        else {
+            b.push(a[4]);
+        }
+        return b;
+    }
+    else if (x<=2/10){
+        let z = Ka.mulberry32(1)
+        let y = z()
+        let a = ["A", "I", "C", "K", "D"];
+        let b = ["AID", "ICK", "CAD", "KID", "CAK"];
+        if (y<=1/5){
+            a.push(b[0]);
+        }
+        else if (y<=2/5){
+            a.push(b[1]);
+        }
+        else if (y<=3/5){
+            a.push(b[2]);
+        }
+        else if (y<=4/5){
+            a.push(b[3]);
+        }
+        else {
+            a.push(b[4]);
+        }
+        return a;
+    }
+    else if (x<=3/10){
+        let z = Ka.mulberry32(1)
+        let y = z()
+        let a = ["A", "O", "G", "M", "P"];
+        let b = ["GAM", "MOP", "PAM", "GAP", "MOG"];
+        if (y<=1/5){
+            a.push(b[0]);
+        }
+        else if (y<=2/5){
+            a.push(b[1]);
+        }
+        else if (y<=3/5){
+            a.push(b[2]);
+        }
+        else if (y<=4/5){
+            a.push(b[3]);
+        }
+        else {
+            a.push(b[4]);
+        }
+        return a;
+    }
+    else if (x<=4/10){return}
+    else if (x<=5/10){return}
+    else if (x<=6/10){return}
+    else if (x<=7/10){return}
+    else if (x<=8/10){return}
+    else if (x<=9/10){return}
+    else 
+}
