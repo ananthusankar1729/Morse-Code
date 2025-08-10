4e7b610cdb68a526ada61575d5a0014e6c4ebf27
oreoruuser <208537580+oreoruuser@users.noreply.github.com>
2025-08-10 12:58:10 +0530

    Replace prompts and alerts with input and button

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,53 +1,98 @@
 import Ka from './ka.js'
 let right = 0;
 let wrong = 0;
+const sky = document.querySelector(".sky");
+const input = document.createElement("input");
+const button = document.createElement("button");
+button.textContent = "Start Game";
+const text = document.createElement("p");
+input.type = "text";
+input.style.border = "2px solid red";
+input.style.width = "200px";
+input.style.height = "30px";
+input.placeholder = "You can type here";
+sky.appendChild(text);
+sky.appendChild(input);
+sky.appendChild(button);
+text.margin = "2px";
+text.textContent = "The lights blink in Morse Code. Type the word you see. Click the button to submit your answer. The following is the Morse Code for some of the letters you will see:";
+text.appendChild(document.createElement("br"));
 function getHumanChoice() {
     const morse = {
-                'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
-                'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
-                'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
-                'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
-                'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
-                'Z': '--..'
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+        'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+        'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+        'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+        'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+        'Z': '--..'
     };
     function morseCode(letter) {
-            return morse[letter];
+        return morse[letter];
     }
     return function (choice) {
-        let userWord ='';
+        let userWord = '';
         let computerArray = choice();
-    for (let i = 0; i < 6; i++) {
-        if (i != 5)
-            prompt("Try typing the morse code of " + computerArray[i] + ", " + morseCode(computerArray[i]) + " in the console");
-        else {
-            const word = computerArray[5].split('').map(morseCode).join(' ');
-            userWord = prompt("Try typing the morse code of " + computerArray[i] + ", " + word + " in the console");
-            let x = [userWord, word];
-            return x;
+        for (let i = 0; i < 6; i++) {
+            if (i != 5) {
+                const textNode = document.createTextNode(computerArray[i] + " is " + morseCode(computerArray[i]) + "  ,  ");
+                text.appendChild(textNode);
+            }
+            else {
+                const word = computerArray[5].split('').map(morseCode).join(' ');
+                text.appendChild(document.createElement("br"));
+                text.appendChild(document.createTextNode("Now enter morse code for the word: " + computerArray[5]));
+                button.textContent = "Submit Answer";
+                button.onclick = function () {
+                    userWord = input.value.trim();
+                    input.value = '';
+                    if (userWord === word) {
+                        right++;
+                        input.style.display = "none";
+                        text.textContent = "Correct! Your score is: " + right
+                        button.textContent = "Continue";
+                        button.onclick = function () {
+                            input.style.display = "inline";
+                            window.gameStarted = true;
+                            game();
+                        };
+                    } else {
+                        wrong++;
+                        input.style.display = "none";
+                        text.textContent = "Wrong! The correct answer was: " + word
+                        button.textContent = "Continue";
+                        window.gameStarted = false;
+                        button.onclick = function () {
+                            input.style.display = "inline";
+                            game();
+                        };
+                    }
+                };
+                let x = [userWord, word];
+                return x;
+            }
         }
-    }
         return userWord;
     }
 }
-function getComputerChoice(){
-    let x = Ka.mulberry32(12345); 
+function getComputerChoice() {
+    let x = Ka.mulberry32(12345);
     let z = Ka.mulberry32(12345);
-    return function (){
+    return function () {
         let w = x();
         let y = z();
-        if (w<=(1/10)) {
+        if (w <= (1 / 10)) {
             let a = ["A", "E", "T", "R", "S"];
             let b = ["ART", "EAR", "TEA", "RAT", "SET"];
-            if (y<=1/5){
+            if (y <= 1 / 5) {
                 a.push(b[0]);
             }
-            else if (y<=2/5){
+            else if (y <= 2 / 5) {
                 a.push(b[1]);
             }
-            else if (y<=3/5){
+            else if (y <= 3 / 5) {
                 a.push(b[2]);
             }
-            else if (y<=4/5){
+            else if (y <= 4 / 5) {
                 a.push(b[3]);
             }
             else {
@@ -55,19 +100,19 @@ function getComputerChoice(){
             }
             return a;
         }
-        else if (w<=2/10){
+        else if (w <= 2 / 10) {
             let a = ["A", "I", "C", "K", "D"];
             let b = ["AID", "ICK", "CAD", "KID", "CAK"];
-            if (y<=1/5){
+            if (y <= 1 / 5) {
                 a.push(b[0]);
             }
-            else if (y<=2/5){
+            else if (y <= 2 / 5) {
                 a.push(b[1]);
             }
-            else if (y<=3/5){
+            else if (y <= 3 / 5) {
                 a.push(b[2]);
             }
-            else if (y<=4/5){
+            else if (y <= 4 / 5) {
                 a.push(b[3]);
             }
             else {
@@ -75,19 +120,19 @@ function getComputerChoice(){
             }
             return a;
         }
-        else if (w<=3/10){
+        else if (w <= 3 / 10) {
             let a = ["A", "O", "G", "M", "P"];
             let b = ["GAM", "MOP", "PAM", "GAP", "MOG"];
-            if (y<=1/5){
+            if (y <= 1 / 5) {
                 a.push(b[0]);
             }
-            else if (y<=2/5){
+            else if (y <= 2 / 5) {
                 a.push(b[1]);
             }
-            else if (y<=3/5){
+            else if (y <= 3 / 5) {
                 a.push(b[2]);
             }
-            else if (y<=4/5){
+            else if (y <= 4 / 5) {
                 a.push(b[3]);
             }
             else {
@@ -95,19 +140,19 @@ function getComputerChoice(){
             }
             return a;
         }
-        else if (w<=4/10){
+        else if (w <= 4 / 10) {
             let a = ["E", "I", "B", "L", "T"];
             let b = ["BET", "BIT", "LET", "LIT", "TIE"];
-            if (y<=1/5){
+            if (y <= 1 / 5) {
                 a.push(b[0]);
             }
-            else if (y<=2/5){
+            else if (y <= 2 / 5) {
                 a.push(b[1]);
             }
-            else if (y<=3/5){
+            else if (y <= 3 / 5) {
                 a.push(b[2]);
             }
-            else if (y<=4/5){
+            else if (y <= 4 / 5) {
                 a.push(b[3]);
             }
             else {
@@ -115,19 +160,19 @@ function getComputerChoice(){
             }
             return a;
         }
-        else if (w<=5/10){
-            let a = [ "E", "O", "H", "W", "F"];
+        else if (w <= 5 / 10) {
+            let a = ["E", "O", "H", "W", "F"];
             let b = ["HOE", "WOE", "HEW", "FOE", "WHO"];
-            if (y<=1/5){
+            if (y <= 1 / 5) {
                 a.push(b[0]);
             }
-            else if (y<=2/5){
+            else if (y <= 2 / 5) {
                 a.push(b[1]);
             }
-            else if (y<=3/5){
+            else if (y <= 3 / 5) {
                 a.push(b[2]);
             }
-            else if (y<=4/5){
+            else if (y <= 4 / 5) {
                 a.push(b[3]);
             }
             else {
@@ -135,19 +180,19 @@ function getComputerChoice(){
             }
             return a;
         }
-        else if (w<=6/10){
-            let a = ["A","U", "J", "Y", "G"];
+        else if (w <= 6 / 10) {
+            let a = ["A", "U", "J", "Y", "G"];
             let b = ["JUG", "GAY", "YAK", "GUY", "JAY"];
-            if (y<=1/5){
+            if (y <= 1 / 5) {
                 a.push(b[0]);
             }
-            else if (y<=2/5){
+            else if (y <= 2 / 5) {
                 a.push(b[1]);
             }
-            else if (y<=3/5){
+            else if (y <= 3 / 5) {
                 a.push(b[2]);
             }
-            else if (y<=4/5){
+            else if (y <= 4 / 5) {
                 a.push(b[3]);
             }
             else {
@@ -155,19 +200,19 @@ function getComputerChoice(){
             }
             return a;
         }
-        else if (w<=7/10){
-            let a = [ "I", "O", "P", "Z", "T"];
+        else if (w <= 7 / 10) {
+            let a = ["I", "O", "P", "Z", "T"];
             let b = ["TIP", "TOP", "PIT", "POT", "ZIP"];
-            if (y<=1/5){
+            if (y <= 1 / 5) {
                 a.push(b[0]);
             }
-            else if (y<=2/5){
+            else if (y <= 2 / 5) {
                 a.push(b[1]);
             }
-            else if (y<=3/5){
+            else if (y <= 3 / 5) {
                 a.push(b[2]);
             }
-            else if (y<=4/5){
+            else if (y <= 4 / 5) {
                 a.push(b[3]);
             }
             else {
@@ -175,39 +220,39 @@ function getComputerChoice(){
             }
             return a;
         }
-        else if (w<=8/10){
+        else if (w <= 8 / 10) {
             let a = ["E", "U", "S", "N", "C"];
             let b = ["USE", "SUN", "NET", "NUT", "CUE"];
-            if (y<=1/5){
+            if (y <= 1 / 5) {
                 a.push(b[0]);
             }
-            else if (y<=2/5){
+            else if (y <= 2 / 5) {
                 a.push(b[1]);
             }
-            else if (y<=3/5){
+            else if (y <= 3 / 5) {
                 a.push(b[2]);
             }
-            else if (y<=4/5){
+            else if (y <= 4 / 5) {
                 a.push(b[3]);
             }
             else {
                 a.push(b[4]);
             }
-            return a;  
+            return a;
         }
-        else if (w<=9/10){
+        else if (w <= 9 / 10) {
             let a = ["A", "I", "M", "L", "V"];
             let b = ["AIM", "VIA", "MIA", "MAL", "MIL"];
-            if (y<=1/5){
+            if (y <= 1 / 5) {
                 a.push(b[0]);
             }
-            else if (y<=2/5){
+            else if (y <= 2 / 5) {
                 a.push(b[1]);
             }
-            else if (y<=3/5){
+            else if (y <= 3 / 5) {
                 a.push(b[2]);
             }
-            else if (y<=4/5){
+            else if (y <= 4 / 5) {
                 a.push(b[3]);
             }
             else {
@@ -216,24 +261,71 @@ function getComputerChoice(){
             return a;
         }
         else {
-            let a = ["O","U", "W", "R", "B"];
+            let a = ["O", "U", "W", "R", "B"];
             let b = ["OUR", "ROW", "WOO", "RUB", "BOW"];
-            if (y<=1/5){
+            if (y <= 1 / 5) {
                 a.push(b[0]);
             }
-            else if (y<=2/5){
+            else if (y <= 2 / 5) {
                 a.push(b[1]);
             }
-            else if (y<=3/5){
+            else if (y <= 3 / 5) {
                 a.push(b[2]);
             }
-            else if (y<=4/5){
+            else if (y <= 4 / 5) {
                 a.push(b[3]);
-            }           
+            }
             else {
                 a.push(b[4]);
             }
-            return a;   
+            return a;
         }
-    }  
-}   
+    }
+}
+function playRound() {
+    let level = 1;
+    let h = getHumanChoice();
+    let i = 0;
+    window.gameStarted = false;
+    return function () {
+        if (window.gameStarted) {
+            button.textContent = "Start Level " + (level + 1);
+            text.textContent = "You are on level " + (level + 1)
+            input.value = '';
+            input.style.display = "none";
+            button.onclick = function () {
+                level++;
+                startLevel();
+            };
+        }
+        else {
+            if (i == 0) {
+                text.textContent = "Welcome to the Morse Code Game! Enter your level of progress: "
+            } else {
+                text.textContent = "Lets retry the Morse Code Game! Press Continue"
+                input.style.display = "none";
+            }
+            i = 1;
+            button.textContent = "Continue";
+            button.onclick = function () {
+                level = parseInt(input.value) || level;
+                input.value = '';
+                window.gameStarted = true;
+                startLevel();
+            };
+        }
+
+        function startLevel() {
+            text.textContent = "Level " + level + " - Here are the morse codes: ";
+            input.style.display = "inline";
+            text.appendChild(document.createElement("br"));
+            let choice = getComputerChoice();
+            for (let i = 1; i < level; i++) {
+                choice();
+            }
+            h(choice);
+        }
+    };
+}
+let game = playRound();
+game();
